ef171e3c3b24246d133c51e38a8a77f581c34b63
Webstir <bpgrandy@gmail.com>
2016-11-17 10:55:43 -0800

    First upload

---
+++ JS/logo.js
@@ -0,0 +1,28 @@
+// Scroll to top
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+window.onscroll = function () {
+    if (pageYOffset >= 800) {
+        document.getElementById('backToTop').style.visibility = "visible";
+    } else {
+ document.getElementById('backToTop').style.visibility = "hidden";
+    }
+};
+
+// Logo mouseover flip
+$(document).on({
+   mouseover: function(event) {
+         $('.navbar li').css("opacity", "1");
+         $('.logo').css("transform", "rotateX(180deg)");
+         $('.logo').css("opacity", "0");
+
+   },
+   mouseleave: function(event) {
+   	   $('.logo').css("transform", "rotateX(0deg)");
+       $('.navbar li').css("opacity", "0");
+       $('.logo').css("opacity", "1");
+   }
+}, '.nav-wrapper')
