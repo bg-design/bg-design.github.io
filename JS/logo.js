87e0f7f7f49f58ecaa7beae17b381b4b15f0e18c
Webstir <bpgrandy@gmail.com>
2016-11-28 12:13:02 -0800

    no hidden nav

--- JS/logo.js
+++ JS/logo.js
@@ -13,33 +13,33 @@ window.onscroll = function () {
 };
 
 // Mouseover nav flip
-$(document).on({
-   mouseover: function(event) {
-        $('.logo').css("transform", "rotateX(180deg)");
-        $('.logo').css("opacity", "0");
-        $('.ham-menu').css("transform", "scale(0,0)");
-        $('.ham-menu').css("opacity", "0");
-        $('.navbar li').css("display", "inline");
-            $('.logo').css("display", "none");
-            $('.ham-menu').css("display", "none");
+// $(document).on({
+//    mouseover: function(event) {
+//         $('.logo').css("transform", "rotateX(180deg)");
+//         $('.logo').css("opacity", "0");
+//         $('.ham-menu').css("transform", "scale(0,0)");
+//         $('.ham-menu').css("opacity", "0");
+//         $('.navbar li').css("display", "inline");
+//             $('.logo').css("display", "none");
+//             $('.ham-menu').css("display", "none");
 
-   },
-   mouseleave: function(event) {
-   	   $('.navbar li').css("display", "none");
-       $('.logo').css("display", "inline");
-       $('.logo').css("opacity", "1");
-       $('.ham-menu').css("display", "inline");
-       $('.ham-menu').css("opacity", "1");
+//    },
+//    mouseleave: function(event) {
+//    	   $('.navbar li').css("display", "none");
+//        $('.logo').css("display", "inline");
+//        $('.logo').css("opacity", "1");
+//        $('.ham-menu').css("display", "inline");
+//        $('.ham-menu').css("opacity", "1");
 
-       setTimeout(function() {
-          $('.logo').css("transform", "rotateX(0deg)");
-          $('.logo').css("opacity", "1");
-          $('.ham-menu').css("transform", "scale(1,1)");
-          $('.ham-menu').css("opacity", "1");
-       },000);
+//        setTimeout(function() {
+//           $('.logo').css("transform", "rotateX(0deg)");
+//           $('.logo').css("opacity", "1");
+//           $('.ham-menu').css("transform", "scale(1,1)");
+//           $('.ham-menu').css("opacity", "1");
+//        },000);
 
-   }
-}, '.nav-wrapper')
+//    }
+// }, '.nav-wrapper')
 
 // Click on hamburger
 function showNav() {
@@ -48,6 +48,7 @@ function showNav() {
   $('.ham-menu').css("transform", "scale(0,0)");
   $('.ham-menu').css("opacity", "0");
   $('.navbar li').css("display", "inline");
+  $('.hamburger').css("border", "none");
 
   setTimeout(function() {
     $('.logo').css("display", "none");
