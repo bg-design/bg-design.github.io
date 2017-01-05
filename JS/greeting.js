8b7399ced58b7122fbf214db24c3b87cf9b7c63f
Webstir <bpgrandy@gmail.com>
2017-01-04 19:41:50 -0800

    greeting update

--- JS/greeting.js
+++ JS/greeting.js
@@ -7,9 +7,12 @@ datetoday = new Date();
 timenow=datetoday.getTime();
 datetoday.setTime(timenow);
 thehour = datetoday.getHours();
-if (thehour > 17) display = "evening";
-else if (thehour >12) display = "afternoon";
-else display = "morning";
-var greeting = ("Good " + display);
+if (thehour > 21 || thehour < 4) display = "Burning the midnight oil?";
+else if (thehour >17) display = "Good evening.";
+else if (thehour >12) display = "Good afternoon.";
+else if (thehour >7) display = "Morning.";
+else display = "Top of the mornin'!";
+var greeting = (display);
 
+// document.getElementById("greeting-title").innerHTML = greeting;
 document.getElementById("greeting").innerHTML = greeting;
