336e7bbf7e594c1d740ea75c67fdcdd42e9a4930
Webstir <bpgrandy@gmail.com>
2018-12-07 23:21:09 -0800

    push it

--- JS/dark-mode2.js
+++ JS/dark-mode2.js
@@ -1,56 +1,35 @@
 
 // AM/PM Dark Mode
-function nightTime() {
-	var time = new Date();
-    var hour = time.getHours();
-    // if ((hour <= 6) || (hour >= 18))
-    if (true)
-    {
-    	$( "p" ).addClass( "dark-mode" );
-    	$( "html" ).addClass( "dark-mode-bg-home" );
-    	$( ".home-link" ).addClass( "dark-mode" );
+// function nightTime() {
+// 	var time = new Date();
+//     var hour = time.getHours();
+//     // if ((hour <= 6) || (hour >= 18))
+//     if (true)
+//     {
+//     	$( "p" ).addClass( "dark-mode" );
+//     	$( "html" ).addClass( "dark-mode-bg-home" );
+//     	$( ".home-link" ).addClass( "dark-mode" );
+//     }
+// ;
+// }
+
+// $( document ).ready(function() {
+// 	nightTime();
+// });
+
+// // Current year for copyright (future-proof)
+// document.getElementById("current-year").innerHTML = (new Date().getFullYear());
+
+// Dark mode switch listener
+
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
     }
-;
-}
-
-$( document ).ready(function() {
-	nightTime();
-});
-
-//// DARK MODE SWITCH
-// Highlight Buttons
-var $highlight = $('.highlight');
-
-function translate(obj) {
-  
-    var $this    = obj.addClass('active'),
-        width    = $this.width(),
-        margin   = $this.outerWidth(true) - $this.outerWidth(),
-        position = $this.offset().left + margin/2;
-
-    $highlight.css({
-        width: parseInt(width) + 'px',
-        transform: 'translate(' + parseInt(position) + 'px' + ')'
-    });
-}
-
-// add transition after page load
-$(window).on('load resize', function() {
-    
-    // wait a little bit to add transition so we don't see on page load
-    setTimeout(function() {
-        $highlight.css({
-            transition: 'all .5s'
-        });
-    }, 100);
-
-    // load highlight on active control
-  translate($('li:first-child'));
-});
-
-// switch to active control on click
-$('li:not(.highlight)').on('click', function() {
-  
-  $('li').removeClass('active');
-  translate($(this));
+  });
 });
