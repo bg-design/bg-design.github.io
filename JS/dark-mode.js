330b2a1f3f7a59ed67cde2b61f665f7c957d7177
Webstir <bpgrandy@gmail.com>
2018-12-14 12:31:21 -0800

    dark mode modal confirmation test

--- JS/dark-mode.js
+++ JS/dark-mode.js
@@ -1,4 +1,59 @@
 
+function darkModeOn() {
+
+    $("#darkModeOff").removeClass("show");
+
+    var snackbar = document.getElementById("darkModeOn");
+    snackbar.className = "show";
+    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 4000);
+  }
+
+  function darkModeOff() {
+
+    $("#darkModeOn").removeClass("show");
+
+    var snackbar = document.getElementById("darkModeOff");
+    snackbar.className = "show";
+    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 4000);
+  }
+
+// Dark mode switch listener
+
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      document.body.classList.add("dark-mode");
+      sessionStorage.setItem('setDarkMode', 'true');
+      darkModeOn ();
+    } else {
+      document.body.classList.remove("dark-mode");
+      sessionStorage.setItem('setDarkMode', 'false');
+      darkModeOff ();
+    }
+  });
+});
+
+var time = new Date();
+var hour = time.getHours();
+if ( sessionStorage.getItem('setDarkMode') === 'true') {
+      document.body.classList.add("dark-mode");
+      document.getElementById("checkbox").checked = true;
+    }
+else if ( sessionStorage.getItem('setDarkMode') === 'false') {
+    document.body.classList.remove("dark-mode");
+    document.getElementById("checkbox").checked = false;
+}
+else if ((hour <= 6) || (hour >= 18)) {
+    document.body.classList.add("dark-mode");
+    document.getElementById("checkbox").checked = true;
+}
+else {
+  document.body.classList.remove("dark-mode");
+  document.getElementById("checkbox").checked = false;
+}
+
 // var time = new Date();
 // var hour = time.getHours();
 // if ( sessionStorage.getItem('setDarkMode') === 'true') {
