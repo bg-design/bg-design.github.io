1a8b068735480477a96e99078be3fb37f318c159
Webstir <bpgrandy@gmail.com>
2018-12-19 10:25:31 -0800

    try this...

--- JS/global.js
+++ JS/global.js
@@ -5,7 +5,6 @@ function includeJS(incFile)
 includeJS('https://code.jquery.com/jquery-latest.js');
 includeJS('JS/header-hide.js');
 includeJS('JS/selection-sharer.js');
-includeJS('JS/contact-modal.js');
 includeJS('JS/featherlight.js');
 includeJS('JS/custom.js');
 includeJS('JS/cookie-monster.js');
