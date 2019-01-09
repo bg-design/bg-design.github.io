0ca52b8f70111c834aa3f951fb9e9138ff8ba9e6
Webstir <bpgrandy@gmail.com>
2019-01-09 13:01:45 -0800

    Enable/disable scroll?

--- JS/contact-modal.js
+++ JS/contact-modal.js
@@ -82,6 +82,7 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
     $('html').css("overflow", "hidden");
+    disableScroll();
 
 }
 
@@ -89,6 +90,7 @@ btn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
     $('html').css("overflow", "auto");
+    enableScroll();
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -96,6 +98,45 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
         $('html').css("overflow", "auto");
+        enableScroll();
     }
+}
+
+// PREVENT SCROLL BEHAVIOR WHILE OVERLAY IS ACTIVE
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
 }
  
